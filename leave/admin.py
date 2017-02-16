9aac5446494f1564ee21f61341440344b05f733b
Luyanda Gcabo <luyandagcabo@gmail.com>
2017-02-16 19:09:11 +0200

    ~ registered the models on admin.py and applied migrations to set the table names explicitly based on class meta.

--- leave/admin.py
+++ leave/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Leave, Employee
 
-# Register your models here.
+
+class LeaveAdmin(admin.ModelAdmin):
+
+    list_display = ['start_date', 'end_date', 'leave_days']
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+
+    list_display = ['start_date', 'leave', 'leave_days']
+
+admin.site.register(Leave, LeaveAdmin)
+admin.site.register(Employee, EmployeeAdmin)
